ebcf6c9d79ee4b38336ae54f99929d1fff2995ac
Jennifer Rothrock <121233385+jeniverede@users.noreply.github.com>
2023-11-20 17:57:48 +0100

    added url for portfolio github

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -27,7 +27,7 @@ export default function Work() {
                         <p className="portfolio__item--title">Costume Blog</p>
                     </a>
 
-                    <a href="https://rothrock-portfolio.netlify.app/" target="blank" className="portfolio__item">
+                    <a href="https://github.com/jeniverede/portfolio.git" target="blank" className="portfolio__item">
                         <img src="/portfolio_code.png" alt="" className="portfolio__img" />
                         <p className="portfolio__item--title">Portfolio - Github</p>
                     </a>
