6bcf8750c73e50082d04f4444e40a86dd0f11ad0
Jennifer Rothrock <jennifer.r.rothrock@gmail.com>
2024-03-14 16:13:28 +0100

    html strong tag

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -6,7 +6,7 @@ export default function Intro() {
         <>
             <section className="intro" id="home">
                 <h1 className="section__title section__title--intro">
-                    Hello, I'm <b>Jennifer Rothrock</b>
+                    Hello, I'm <strong>Jennifer Rothrock</strong>
                 </h1>
                 <p className="section__subtitle section__subtitle--intro">frontend web/app developer + ui designer</p>
                 <img src="Jen headshot purple.jpg" alt="image of Jen" className="intro__img" />
