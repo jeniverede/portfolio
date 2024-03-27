2153bff253b3b15a7d3cb37db5ce8a1931b93ed0
Jennifer Rothrock <jennifer.r.rothrock@gmail.com>
2024-03-27 22:12:16 +0100

    Github Portfolio.jsx

--- src/components/GithubPortfolio.jsx
+++ src/components/GithubPortfolio.jsx
@@ -5,10 +5,10 @@ export default function GithubPortfolio() {
     return (
         <>
             <section className="intro">
-                <h1 className="section__title section__title--intro">
+                <h1>
                     <strong>Portfolio - Github</strong>
                 </h1>
-                <p className="section__subtitle section__subtitle--intro">Github repo for portfolio website</p>
+                <p>Github repo for portfolio website</p>
                 <img src="/portfolio_code.png" alt="Collection Digital mockups" className="intro__img" />
             </section>
 
