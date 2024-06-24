c4f4edc3286c1d8d7c9964c654499f2c9b215f3f
Jennifer Rothrock <jennifer.r.rothrock@gmail.com>
2024-06-24 13:33:11 +0200

    design update

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -6,19 +6,19 @@ export default function Services() {
             <h2 className="section__title section__title--services">What you get</h2>
             <div className="services">
                 <div className="service">
-                    <h3 className="accent">Software Development</h3>
+                    <h3>Software Development</h3>
                     <p className="left_align">My motivation to transition into coding came from a desire to go beyond designing digital products. I wanted to understand and be part of the process of building these products from the ground up.</p>
                 </div>
 
                 <div className="service">
-                    <h3 className="accent">UI Design</h3>
+                    <h3>UI Design</h3>
                     <p className="left_align">With 25 years of experience in design and product development, I
                     bring my  expertise in creating visually appealing and user-friendly digital interfaces. I have a unique blend of creativity, aesthetic taste, and business perspective. 
                     </p>
                 </div>
 
                 <div className="service">
-                    <h3 className="accent">Professionalism</h3>
+                    <h3>Professionalism</h3>
                     <p className="left_align">I have essential skills in communication, teamwork, organisation and problem-solving. As a person I am friendly, reliable and work well on a team or independently.</p>
                 </div>
             </div>
