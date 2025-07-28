8ad0c40a56834b481db837125db4bd38b29b42e5
Jennifer Rothrock <jennifer.r.rothrock@gmail.com>
2025-07-28 17:22:39 +0200

    update services page

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -4,19 +4,19 @@ export default function Services() {
     return (
         <div className="my-services" id="services">
             <header>
-                <h2 className="section__title section__title--services">What you get</h2>
+                <h2 className="section__title section__title--services">Skills</h2>
             </header>
 
             <article className="services">
                 <section className="service">
-                    <h3>Software Development</h3>
+                    <h3>Frontend Software Development</h3>
                     <p className="left_align">My motivation to transition into coding came from a desire to go beyond designing digital products. I wanted to understand and be part of the process of building these products from the ground up.</p>
                 </section>
 
                 <section className="service">
-                    <h3>UI Design</h3>
+                    <h3>UI/UX Design</h3>
                     <p className="left_align">With 25 years of experience in design and product development, I
-                        bring my  expertise in creating visually appealing and user-friendly digital interfaces. I have a unique blend of creativity, aesthetic taste, and business perspective.
+                        bring my expertise in creating visually appealing and user-friendly digital interfaces. I have a unique blend of creativity, aesthetic taste, and business perspective.
                     </p>
                 </section>
 
