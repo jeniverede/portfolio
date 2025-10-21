22af4bbad5daae399596bf4f12517c848a0efafc
Jennifer Rothrock <jennifer.r.rothrock@gmail.com>
2025-10-21 16:52:04 +0200

    100 percent image size

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -17,7 +17,7 @@ export default function About() {
                 </p>
             </article>
 
-            <img src="Jen computer.jpg" alt="image Jen working" className="about-me__img" style={{ width: "30%", height: "auto" }}
+            <img src="Jen computer.jpg" alt="image Jen working" className="about-me__img" style={{ width: "100%", height: "auto" }}
  />
         </section>
     );
