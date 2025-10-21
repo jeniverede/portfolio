062948766644078f88562a7e49d61b49290f3480
Jennifer Rothrock <jennifer.r.rothrock@gmail.com>
2025-10-21 17:05:58 +0200

    rewrite css for all device sizes

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -17,8 +17,7 @@ export default function About() {
                 </p>
             </article>
 
-            <img src="Jen computer.jpg" alt="image Jen working" className="about-me__img" style={{ width: "150%", height: "auto" }}
- />
+            <img src="Jen computer.jpg" alt="image Jen working" className="about-me__img"/>
         </section>
     );
 }
