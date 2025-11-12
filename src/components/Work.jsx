db3ef67379e5aea60f15b1f4e21c84e8f10eb5f8
Jennifer Rothrock <jennifer.r.rothrock@gmail.com>
2025-11-12 14:30:07 +0100

    move UIUX to top of work section

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -7,6 +7,25 @@ export default function Work() {
             <section className="my-work" id="work">
 
                 <h2 className="section__title section__title">My Work</h2>
+                <h3 className="section__title section__title--work">UI/UX Design</h3>
+
+                <article className="portfolio">
+                    <a href="museumguideberlin" target="_blank" rel="noreferrer" className="portfolio__item">
+                        <img src="/MGB cover img 2.jpg" alt="Museum Guide Berlin" className="portfolio__img" />
+                        <p className="portfolio__item--title">Museum Guide Berlin</p>
+                    </a>
+
+                    <a href="tierheimberlin" target="_blank" rel="noreferrer" className="portfolio__item">
+                        <img src="/Tierheim Berlin cover img 2.jpg" alt="Tierheim Berlin" className="portfolio__img" />
+                        <p className="portfolio__item--title">Tierheim Berlin</p>
+                    </a>
+
+                    <a href="secondhandshadows" target="_blank" rel="noreferrer" className="portfolio__item">
+                        <img src="/ShS Card Mockup iOS 401.jpg" alt="Second Hand Shadows" className="portfolio__img" />
+                        <p className="portfolio__item--title">Second hand Shadows</p>
+                    </a>
+                </article>
+                
                 <h3 className="section__title section__title--work">Web Development</h3>
 
                 <article className="portfolio">
@@ -33,25 +52,6 @@ export default function Work() {
                         <p className="portfolio__item--title">Mourning, Memory, Memento</p>
                     </a>
                     {/* https://mourning-memory-memento.netlify.app */}
-                </article>
-
-                <h3 className="section__title section__title--work">UI/UX Design</h3>
-
-                <article className="portfolio">
-                    <a href="museumguideberlin" target="_blank" rel="noreferrer" className="portfolio__item">
-                        <img src="/MGB cover img 2.jpg" alt="Museum Guide Berlin" className="portfolio__img" />
-                        <p className="portfolio__item--title">Museum Guide Berlin</p>
-                    </a>
-
-                    <a href="tierheimberlin" target="_blank" rel="noreferrer" className="portfolio__item">
-                        <img src="/Tierheim Berlin cover img 2.jpg" alt="Tierheim Berlin" className="portfolio__img" />
-                        <p className="portfolio__item--title">Tierheim Berlin</p>
-                    </a>
-
-                    <a href="secondhandshadows" target="_blank" rel="noreferrer" className="portfolio__item">
-                        <img src="/ShS Card Mockup iOS 401.jpg" alt="Second Hand Shadows" className="portfolio__img" />
-                        <p className="portfolio__item--title">Second hand Shadows</p>
-                    </a>
                 </article>
 
             </section >
