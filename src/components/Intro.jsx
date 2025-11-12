411264926237644d023feccec37a4c2b66111fa0
Jennifer Rothrock <jennifer.r.rothrock@gmail.com>
2025-11-12 14:34:00 +0100

    re-write intro

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -16,7 +16,7 @@ export default function Intro() {
                     <article className="intro-descr">
                         <h2 className="main-heading"> Hello, my name is <strong>Jennifer Rothrock</strong></h2>
                         <br />
-                        <p className="paragraph">I&apos;m a Web Designer &amp; Frontend Developer who loves clean, simple & unique designs. </p>
+                        <p className="paragraph">I&apos;m a UI/UX, Web Designer &amp; Frontend Developer who loves clean, simple & unique designs. </p>
                     </article>
                 </div>
             </section>
