f124b98d7ccf6d02ef44e07980cc46d65271d7f9
Jennifer Rothrock <jennifer.r.rothrock@gmail.com>
2025-11-17 14:07:35 +0100

    added spacing between Imp PP

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -20,25 +20,25 @@ export default function Footer() {
             </ul>
 
             <div className="container_impressum">
-  <a
-    className="social-list__link"
-    href="/impressum"
-    target="_blank"
-    rel="noreferrer"
-  >
-    Impressum
-  </a>
-  
-  <a
-    className="social-list__link"
-    href="/privacy-policy"
-    target="_blank"
-    rel="noreferrer"
-    style={{ marginLeft: "1rem" }} // optional spacing between links
-  >
-    Privacy Policy
-  </a>
-</div>
+                <a
+                    className="social-list__link"
+                    href="/impressum"
+                    target="_blank"
+                    rel="noreferrer"
+                >
+                    Impressum
+                </a>
+
+                <a
+                    className="social-list__link"
+                    href="/privacy-policy"
+                    target="_blank"
+                    rel="noreferrer"
+                    style={{ marginLeft: "3rem" }} // optional spacing between links
+                >
+                    Privacy Policy
+                </a>
+            </div>
 
         </footer>
     );
