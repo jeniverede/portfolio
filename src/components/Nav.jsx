6ed1d558de1b430c8ff08cbf0faac97e9c2c72e8
Jennifer Rothrock <jennifer.r.rothrock@gmail.com>
2025-11-19 16:29:25 +0100

    update nav item Resume

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -63,7 +63,7 @@ export default function Nav() {
                             <li className="nav__item"><Link to="/#work" className="nav__link">My Work</Link></li>
                             <li className="nav__item"><Link to="/#references" className="nav__link">References</Link></li>
                             <li className="nav__item"><Link to="/#contact" className="nav__link">Contact</Link></li>
-                            <li className="nav__item"><Link to="/#resume" className="nav__link">Resume</Link></li>
+                            <li className="nav__item"><Link to="/resume" className="nav__link">Resume</Link></li>
                             
                         </div>
                     </ul>
